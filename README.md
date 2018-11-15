3410c47e6b384b5c461ff32e0179f8d9f712912e
Jaakko Reinvall <reinvall.jaakko@gmail.com>
2018-11-15 15:59:00 +0200

    add data loading

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # improved-WGANs
 Pytorch implementation of WGAN with gradient penalty
+
+## Usage
+1. ```python3 main.py --datapath /path/to/data```
---
+++ batch_generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+def batch_generator(batch_size, phase, datapath):
+    if phase == 'train':
+        train = True
+    elif phase == 'test':
+        train = False
+    else:
+        raise ValueError
+    data_transforms = transforms.Compose([transforms.ToTensor()])  
+    dataset = datasets.MNIST(datapath, train=train, transform = data_transforms, download=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    iterator = enumerate(dataloader)
+    return iterator
